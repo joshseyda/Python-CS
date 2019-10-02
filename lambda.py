c8dfc7eb9219ce64924c72a04944b4bfc9210ce8
joshseyda <josh.seyda@gmail.com>
2019-10-02 11:55:14 -0400

    intro to lambda functions

---
+++ lambda.py
@@ -0,0 +1,9 @@
+# regular function
+def square_number(x):
+    res = x ** 2
+    return res
+# lambda function
+square = lambda x: x ** 2
+# results
+print('square_number(4): {}'.format(square_number(4)))
+print('square lambda: {}'.format(square(4)))
